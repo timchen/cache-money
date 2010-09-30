1b7cef3ff21ddc09cb558240d42eb6b7c92e0672
Ashley Martens <ashleym_72@yahoo.com>
2010-09-30 12:28:13 -0700

    Make the spec helper more bullet proof.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,30 @@ require File.join(dir, '../config/environment')
 require 'spec'
 require 'pp'
 require 'cache_money'
-#require 'memcache'
 require 'memcached'
-require 'memcached_wrapper'
+require 'memcache'
+require 'ruby-debug'
 
 Spec::Runner.configure do |config|
+  config.after :suite do
+    IO.popen("kill #{$memcached_pid}")
+  end
+  
   config.mock_with :rr
   config.before :suite do
     load File.join(dir, "../db/schema.rb")
 
-    config = YAML.load(IO.read((File.expand_path(File.dirname(__FILE__) + "/../config/memcached.yml"))))['test']
-    $memcache = MemcachedWrapper.new(config["servers"].gsub(' ', '').split(','), config)
+    IO.popen('memcached -d -l 127.0.0.1 -p 11212')
+    IO.popen('ps -ef | grep memcached | grep 11212') { |f| $memcached_pid = f.gets.split[1].to_i }
+    p $memcached_pid
+
+    config = { 'ttl'       => 604800,
+               'namespace' => 'cache',
+               'sessions'  => false,
+               'debug'     => true,
+               'servers'   => '127.0.0.1:11212'
+    }
+    $memcache = MemCache.new(config["servers"].gsub(' ', '').split(','), config)
     $lock = Cash::Lock.new($memcache)
   end
 
@@ -30,23 +43,27 @@ Spec::Runner.configure do |config|
       is_cached :repository => Cash::Transactional.new($memcache, $lock)
     end
 
-    Character = Class.new(ActiveRecord::Base)
-    Story = Class.new(ActiveRecord::Base)
-    Story.has_many :characters
+    Character = Class.new(ActiveRecord::Base) unless defined?(Character)
+    unless defined?(Story)
+      Story = Class.new(ActiveRecord::Base)
+      Story.has_many :characters
 
-    Story.class_eval do
-      index :title
-      index [:id, :title]
-      index :published
+      Story.class_eval do
+        index :title
+        index [:id, :title]
+        index :published
+      end
     end
 
-    Short = Class.new(Story)
-    Short.class_eval do
-      index :subtitle, :order_column => 'title'
+    unless defined?(Short)
+      Short = Class.new(Story)
+      Short.class_eval do
+        index :subtitle, :order_column => 'title'
+      end
     end
 
-    Epic = Class.new(Story)
-    Oral = Class.new(Epic)
+    Epic = Class.new(Story) unless defined?(Epic)
+    Oral = Class.new(Epic)  unless defined?(Oral)
 
     Character.class_eval do
       index [:name, :story_id]
